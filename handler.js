01564fca8c7d2245c906dcd5dc9adc55197d6a09
Jeremias <moraesjeremias@Jeremiass-MacBook-Pro.local>
2020-11-26 06:45:15 -0300

    mangoodb-function-premium project init

---
+++ handler.js
@@ -0,0 +1,37 @@
+const R = require("ramda")
+
+const mangoDBOperationsPremium = async (event) => {
+  let assignedFoods = [];
+  try {
+    const s3EventObject = R.pathOr(
+      null,
+      ["requestPayload", "requestPayload","Records", [0], "s3"],
+      event
+    );
+    if (!s3EventObject)
+      throw new IncomingS3EventNotFound(
+        `Evento de upload não contém informações do S3 no eventId: ${event.requestContext.requestId}`
+      );
+
+    const objectKeys = R.path(["object", "key"], s3EventObject);
+    const bucketName = R.path(["bucket", "name"], s3EventObject);
+    const userId = R.split("/", objectKeys)[1];
+    const photo = R.split("/", objectKeys)[2];
+
+    const labelList = R.pathOr(null, ["responsePayload", "responsePayload", "CustomLabels"], event);
+    if (!labelList)
+      throw new RekoNotFoundExcpetion(
+        `Não foi possível identificar os objetos do eventId: ${event.requestContext.requestId}`
+      );
+    labelList.map(async label => {
+        const customLabelAccuracy = R.pathOr(null, ["Confidence"], label)
+        if(customLabelAccuracy > 40){
+            const foodDBResponse = 
+        }
+    })
+  } catch(error){
+    console.log(error)   
+  }
+};
+
+module.exports = mangoDBOperationsPremium
---
+++ index.js
@@ -0,0 +1,4 @@
+const premiumEventMockk = require("./tests/__mocks__/premiumEventMockk.json")
+const mangoDBOperationsPremium = require("./handler")
+
+mangoDBOperationsPremium(premiumEventMockk)
